214c4cde49cabc09844754f004d965d9f33ad8cc
Eyvind Johansson <eyvindj@gmail.com>
2024-11-22 21:29:01 +0100

    updated runcode

--- R/runcode_test.R
+++ R/runcode_test.R
@@ -1,19 +1,17 @@
 
 #'  #Prepared code to run the included test image
-#'
-#'
-#'
 
 #run code to import test image to object
-image_read("~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test2.jpg")
-testimage <- "~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test.jpg"
-image_read(testimage )
+
+# run code to test and display gray and twocolor separately
+image_read("~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test.jpg")
+filter_grayscale("~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test.jpg")
+filter_twocolor("~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test.jpg",
+                color1="yellow", color2="purple")
+
 
 testimage <- "~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test2.jpg"
 image_read(testimage )
-# run code to test and display gray and twocolor separately
-filter_grayscale("~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test.jpg")
 filter_grayscale(testimage)
-filter_twocolor("~/Rprogrammingversioncontrol/VectorizedColorFilter/SourceFiles/test.jpg")
-filter_twocolor(testimage)
+filter_twocolor(testimage, color1="yellow", color2="lightblue")
 
